16bea3c90ec3a088edb019d1e3077cdcaf1dfe90
rkoll55 <rohankollambalath@gmail.com>
2023-01-25 23:10:13 +1000

    Documentation

--- trader.py
+++ trader.py
@@ -1,8 +1,38 @@
-from scipy import constants
-import time 
+#define asset
 
-for i in range (20):
-    x = input("")
+#INITIAL CHECK
+#check the position: check if we have open position with asset
+    #IN asset (string)
+    #OUT True (exists)/False (does not)
+#check if position is tradable: check of asset is tradable with broker
+    #IN asset (string)
+    #OUT True (exists)/False (does not)
 
-    print(x)
-    
+#GENERAL TREND
+#load 30 minute candles: demand API 30 minute candles
+    #IN: asset, time range, candle size
+    #OUT: 30 min candles
+#perform general trend analysis
+
+#LOOP
+#load 5 minute candles
+#perfrom instant trend analysis
+#perform RSI analysis
+#perform stochastic analysis
+
+#SUBMIT ORDER
+#ENTER POSITION
+#submit order 
+#check position
+
+#ENTER POSITION MODE (LOOP)
+#check take profit
+#check stop loss
+#check stoch crossing 
+
+#GET OUT
+#check order
+#check position 
+
+#rerun code 
+print("This is my trading bot")
