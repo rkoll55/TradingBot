aef51722855018922f7bd894460906170e1db50d
rkoll55 <rohankollambalath@gmail.com>
2023-01-26 13:44:08 +1000

    completed logger

--- logger.py
+++ logger.py
@@ -1,3 +1,18 @@
 import logging 
+import os 
+from datetime import datetime
 
-logging.basicConfig(filename='bot.log', format = '%(asctime)s - %(levelname)s: %(message)s')
+logs_path = './logs/'
+try:
+    os.mkdir(logs_path)
+except OSError:
+    print("Already exists")
+else:
+    print("made log dir")
+
+now = datetime.now().strftime("%Y%m%d_%H%M%S")
+log_name = now + '.log'
+currentLog_path = logs_path + log_name 
+logging.basicConfig(filename=currentLog_path, format = '%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info("Example error message!")
